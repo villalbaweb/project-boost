602d640bf958774197c77853bc5d0c6c3033f7a1
villalbaweb <villalbaweb@gmail.com>
2020-10-17 00:15:42 -0500

    Adding first commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // config
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust = 100f;
+
+    // cache
+    Rigidbody _rigidbody;
+
+    void Awake() 
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            _rigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+
+    }
+
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+
+    }
+
+    private void ApplyRotation(float rotationValue)
+    {
+        _rigidbody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationValue * Time.deltaTime);
+        _rigidbody.freezeRotation = false;
+    }
+}
