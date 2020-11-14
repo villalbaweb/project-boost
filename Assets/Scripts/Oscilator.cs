7be740f1c08369cbfc0cc4ccb015a5b23ad99961
villalbaweb <villalbaweb@gmail.com>
2020-11-14 14:36:14 -0600

    Adding Oscilator script to make obstacles move

---
+++ Assets/Scripts/Oscilator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Oscilator : MonoBehaviour
+{
+    // config
+    [Range(0, 1)]
+    [SerializeField] float movementFactor;
+    [SerializeField] Vector3 movementVector;
+
+    // state
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
